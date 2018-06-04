c9d403cffe6e84efd5a091bd50250b6061940def
Tori Gonzalez <tori@nexmatix.com>
2018-06-04 08:01:00 -0500

    Completed assignment.  Finally!

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -4,6 +4,7 @@ import org.launchcode.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
@@ -22,6 +23,26 @@ public class SearchController {
         return "search";
     }
 
+    @RequestMapping(value="results")
+    public String searchJobsByColumnAndValue(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+        if (searchType.equals("all")) {
+            ArrayList<HashMap<String,String>> jobs = JobData.findByValue(searchTerm);
+            model.addAttribute("title", "Search for " + searchTerm + " in All Jobs");
+            model.addAttribute("jobs", jobs);
+            return "list-jobs";
+        } else {
+            ArrayList<HashMap<String,String>> items = JobData.findByColumnAndValue(searchType, searchTerm);
+            model.addAttribute("title", "Search for " + searchTerm + " in " + searchType + " field");
+            model.addAttribute("column", searchType);
+            model.addAttribute("items", items);
+            return "list-column";
+        }
+    }
+
+
     // TODO #1 - Create handler to process search request and display results
 
+
+
 }
+
